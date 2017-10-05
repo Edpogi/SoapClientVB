180c4929b7b10a3da1f201c264518860cc4f6cc0
edpogi19 <edpogi19@gmail.com>
2017-10-05 15:46:19 +0800

    Initial Commit

---
+++ SoapClient/Integration/Customer.vb
@@ -0,0 +1,21 @@
+﻿Option Infer On
+Imports SoapClient.TestSOAP
+Imports PX.Soap.Helper
+Public Class Customer
+    Public Sub Customer(ByRef Context As Screen)
+        Dim CustomerID As String = "EDPADI"
+        Dim CustomerName As String = "Ed Padilla"
+        Dim CustomerEmail As String = "edpadilla2@yahoo.com"
+        Dim CustomerAddress As String = "San Juan City"
+
+        Console.WriteLine("Creating Customer Record...")
+
+        Dim CustSchema As RB202000Content = PX.Soap.Helper.GetSchema(Of RB202000Content)(Context)
+
+        Dim Save_CustomerID As Command = New Command()
+
+
+
+    End Sub
+
+End Class
---
+++ SoapClient/Module1.vb
@@ -0,0 +1,24 @@
+﻿Imports SoapClient.TestSOAP
+Module Module1
+
+    Sub Main()
+        Using Context As New Screen
+            Context.CookieContainer = New Net.CookieContainer()
+            Context.Url = My.Settings.SoapClient_TestSOAP_Screen
+            Context.Login(My.Settings.Login, My.Settings.Password)
+
+            Try
+
+            Catch ex As Exception
+                Console.WriteLine(ex)
+                Console.WriteLine()
+                Console.WriteLine("Press Any key to continue")
+                Console.ReadLine()
+
+            Finally
+                Context.Logout()
+            End Try
+        End Using
+    End Sub
+
+End Module
